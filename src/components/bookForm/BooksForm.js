478640ea0a59e468d7f9f8f78878dd7e4cc93cda
phalado <phalado@gmail.com>
2020-04-03 17:38:41 -0300

    Correct a linter error

--- src/components/bookForm/BooksForm.js
+++ src/components/bookForm/BooksForm.js
@@ -1,18 +1,21 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <form>
-      <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" />
-      <label htmlFor="category">Category</label>
-      <select id="category">
-        {
-          categories.map(item => (<option key={item} value={item}>{item}</option>))
-        }
-      </select>
+      <label htmlFor="title">
+        Title
+        <input type="text" id="title" name="title" />
+      </label>
+      <label htmlFor="category">
+        Category
+        <select id="category">
+          {
+            categories.map(item => (<option key={item} value={item}>{item}</option>))
+          }
+        </select>
+      </label>
       <button type="submit">Submit</button>
     </form>
   );
